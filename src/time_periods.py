913d64d9d8f1742b303c37fdea72b42c8b84a0e5
Jace Keagy <jacekeagy22@gmail.com>
2025-11-06 10:02:18 -0600

    Added time_periods skeleton

---
+++ src/time_periods.py
@@ -0,0 +1,27 @@
+# File: src/time_periods.py
+# Description: Initial code skeleton for time_periods.py (Organizing slots of the day).
+# Programmer(s): Jace Keagy, K Li, Lan Lim, Jenna Luong, Kit Magar, Bryce Martin
+# Created: 2025-11-26
+
+# Slots dict for periods.
+slots = {
+  "morning":   "[05:00, 12:00]",
+  "afternoon": "[12:00, 17:00]",
+  "evening":   "[17:00, 21:00]",
+  "night":     "[21:00, 04:00]"
+}
+
+# Gets period with time inserted.
+def determine_period(insert_time_here):
+    # Loop through slots until correct given then return.
+    return
+
+# Returns value from slots dict key.
+def times_for_slot(insert_dict_key_here):
+    # Call key's value.
+    return
+
+# Gets next slot
+def next_slot(insert_current_slot_here):
+    # Progresses to next time slot.
+    return
